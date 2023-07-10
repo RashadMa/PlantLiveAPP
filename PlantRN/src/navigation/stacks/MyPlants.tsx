f9c0bc2861e18667a49a11df638aaf1dcf93e8bb
rashadma <rashadkhm@code.edu.az>
2023-07-10 12:26:23 +0400

    HEHE

---
+++ PlantRN/src/navigation/stacks/MyPlants.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MyPlants = () => {
+  return (
+    <View>
+      <Text>MyPlants</Text>
+    </View>
+  )
+}
+
+export default MyPlants
+
+const styles = StyleSheet.create({})
